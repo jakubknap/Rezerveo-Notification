f1a5b671bb9f5323eff778741059ba08b9bf082a
Jakub Knap <jknap40@gmail.com>
2025-10-13 16:29:44 +0200

    Initial commit

---
+++ src/main/java/pl/rezerveo/notification/RezerveoNotificationApplication.java
@@ -0,0 +1,12 @@
+package pl.rezerveo.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezerveoNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RezerveoNotificationApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/rezerveo/notification/RezerveoNotificationApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.rezerveo.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezerveoNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
